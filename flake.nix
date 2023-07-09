417dca27973aba849083b5012f3dd0ba71270dd7
Walther Chen <walther.chen@gmail.com>
2023-07-08 23:44:21 -0400

    update ols to nightly in flake.nix

--- flake.nix
+++ flake.nix
@@ -16,8 +16,7 @@
             owner = "odin-lang";
             repo = "Odin";
             rev = "${version}";
-            # How is this determined?
-            sha256 = "sha256-qEewo2h4dpivJ7D4RxxBZbtrsiMJ7AgqJcucmanbgxY=";
+            sha256 = "sha256-ksCK1Qmjbg5ZgFoq0I4cjrWaCxd+UW7f1NLcSjCPMwE=";
           };
 
           nativeBuildInputs = with super; [ makeWrapper which ];
@@ -33,10 +32,24 @@
         });
       };
 
+      ols-overlay = self: super: {
+        ols = super.ols.overrideAttrs (old: rec {
+          version = "nightly-2023-07-08-0af33a6";
+          src = super.fetchFromGitHub {
+            owner = "DanielGavin";
+            repo = "ols";
+            rev = "0af33a64747670545eaf31d1449586ff8f08b0f9";
+            sha256 = "sha256-MNl3mcVWG4Tc4xgb1bnkCZu+rZlgLh2W+sr2s2klpm4=";
+          };
+
+          installPhase = old.installPhase;
+        });
+      };
+
       pkgs = import nixpkgs {
         inherit system;
         overlays = [
-          (odin-overlay)
+          (odin-overlay) (ols-overlay)
         ];
       };
 
